1ef910b497d1575bef5db430127305ada9766a86
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2025-10-13 23:54:08 +0600

    presize the fetching data into card and render it in display

--- src/Component/Countries/Countries.jsx
+++ src/Component/Countries/Countries.jsx
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
 
+import "./Countries.css";
+
 export default function Countries() {
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
     fetch(
-      "https://restcountries.com/v3.1/all?fields=name,capital,region,subregion,population,flags,currencies,languages"
+      "https://restcountries.com/v3.1/all?fields=name,capital,region,subregion,population,flags,currencies,languages,cca2,cca3"
     )
       .then((responseOfCountries) => responseOfCountries.json())
       .then((data) => setCountries(data));
   }, []);
+
   return (
     <>
-      {countries.map((country) => (
-        <Country country={country}></Country>
-      ))}
+      <div className="card-container">
+        {countries.map((country) => (
+          <Country key={country.cca3} country={country}></Country>
+        ))}
+      </div>
     </>
   );
 }
--- src/Component/Country/Country.jsx
+++ src/Component/Country/Country.jsx
@@ -1,8 +1,17 @@
+import "./Country.css";
 export default function Country({ country }) {
+  const { flags, name, languages, population } = country;
   console.log(country);
   return (
     <>
-      <p></p>
+      <div className="card">
+        <figure className="card-img">
+          <img src={flags.png} alt="" />
+        </figure>
+        <h1>{name.common}</h1>
+        <p>Language: {languages.lit}</p>
+        <p>Population: {population}</p>
+      </div>
     </>
   );
 }
